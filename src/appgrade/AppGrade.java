6b114196628ae117869a9826f168feb6ca092b09
344_14 <344_14@DESKTOP-AONLHM0>
2025-06-06 16:02:03 +0700

    11

--- src/appgrade/AppGrade.java
+++ src/appgrade/AppGrade.java
@@ -4,6 +4,8 @@
  */
 package appgrade;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 344_14
@@ -14,6 +16,41 @@ public class AppGrade {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int affective,work,exam,total;
+        double grade = 0;
+        Scanner scan = new Scanner(System.in);
+        do{
+        System.out.print("Enter affective : ");
+        affective = scan.nextInt();
+        }while(affective>20);
+        do{
+        System.out.print("Enter work : ");
+        work = scan.nextInt();
+        }while(work>40);
+        do{
+            System.out.print("Enter work : ");
+        exam = scan.nextInt();
+        }while(exam>40);
+        total = affective+exam;
+        if(total<50){
+           grade=0.0;
+        }else if (total<=54){
+            grade=1.0;
+        }else if (total<=59){
+            grade=1.5;
+        }else if (total<=64){
+            grade=2.0;
+        }else if (total<=69){
+            grade=2.5;
+        }else if (total<=74){
+            grade=3.0;
+        }else if (total<=79){
+            grade=3.5;
+        }else if (total<=100){
+            grade=4.0;
+        }
+        System.out.println("total score : "+total);
+        System.out.println("You have grade "+grade);
         // TODO code application logic here
     }
     
